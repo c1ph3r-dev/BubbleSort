8c5cc60c5544260a0378f44be1b14200e1455a36
Jonathan Rampersad <c1ph3r.dev@gmail.com>
2022-06-17 07:45:41 -0400

    Optimized algorithim

--- main.cpp
+++ main.cpp
@@ -9,21 +9,28 @@ void Swap(int& x, int& y)
 
 void BubbleSort(int* array, int size)
 {
+    bool isSorted;
     for (int i = 0; i < size; i++)
     {
-        for (int j = 1; j < size; j++)
+        isSorted = true;
+        for (int j = 1; j < size - i; j++)
         {
             if(array[j] < array[j-1])
-                Swap(array[j], array[j-1]);
+            {
+               Swap(array[j], array[j-1]);
+               isSorted = false;
+            }
         }
+        if(isSorted)
+            return;
     }
 }
 
 int main()
 {
-    int test[] = {8, 7, 6, 5, 4, 3, 2, 1};
+    int test[] = {6, 5, 4, 3, 2, 1, 7, 8};
 
-    BubbleSort(test, 8);
+    BubbleSort(test, sizeof(test)/sizeof(test[0]));
     for (int i = 0; i < 8; i++)
         std::cout << test[i] << std::endl;
 
